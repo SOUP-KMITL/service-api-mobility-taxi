83011f28b734e624645ff8fe04415e44599ed7f3
Parnmet <mos_t_@hotmail.com>
2018-02-21 00:11:00 +0700

    Add getTaxiByLocation logic && previous at res.send() problem. solved by change middleware connect to express

--- controllers/DefaultService.js
+++ controllers/DefaultService.js
@@ -20,29 +20,49 @@ exports.getTaxiByLocation = function(args, res, next) {
       })
     })
   }
+
+  const getData = () => {
+    return new Promise((resolve, reject) => {
+      db.taxiData.find().sort({ '_id': -1 }).limit(1).toArray(function (err, doc) {
+        resolve(doc[0])
+      })
+    })
+  }
+
   getPlace().then((geolocation) => {
     let coord = geolocation.split(',')
-    let lat = parseFloat(coord[0])
-    let long = parseFloat(coord[1])
-    console.log(lat)
-    console.log(long)
+    let location_lat = parseFloat(coord[0])
+    let location_long = parseFloat(coord[1])
+    let new_taxi_json = {}
+    getData().then((data) => {
+      let new_taxi_array = []
+      let taxi_json_array = data.data
+      try {
+        for (var i in taxi_json_array) {
+          if (taxi_json_array[i].gps != null) {
+            if (taxi_json_array[i].gps.recv_ts >= time_now.toISOString()) {
+              //service_status == 1 maybe has passenger
+              if (taxi_json_array[i].service.passenger == null) {
+                let taxi_long = taxi_json_array[i].gps.loc.coordinates[0]
+                let taxi_lat = taxi_json_array[i].gps.loc.coordinates[1]
+                if (Math.abs(location_lat - taxi_lat) <= 0.05 && Math.abs(location_long - taxi_long) <= 0.05) {
+                  delete taxi_json_array[i].service
+                  new_taxi_array.push(taxi_json_array[i])
+                }
+              }              
+            }
+          }
+        }
+      }
+      catch (e) {
+        console.log(e)
+        console.log('Error at id ' + taxi_json_array[i].id)
+      }
+      new_taxi_json.current_gps = new_taxi_array
+      res.send(new_taxi_json)
+      res.end();
+    })
   })
-  // const getPlace = new Promise((resolve, reject) => {
-  //   var options = {
-  //     method: 'GET',
-  //     url: 'http://203.154.59.55:5005/getAllLocations',
-  //   };
-  //   request(options, function (error, response, body) {
-  //     if (error) throw new Error(error);
-  //     resolve(body.place)
-  //   });
-  // })
-
-  // getPlace().then((place) => {
-  //   place.
-  // })
-
-  res.end();
 }
 
 exports.getTaxiCurrentGps = function(args, res, next) {
@@ -69,11 +89,11 @@ exports.getTaxiCurrentGps = function(args, res, next) {
         if (taxi_json_array[i].gps != null) {
           if (taxi_json_array[i].gps.recv_ts >= time_now.toISOString()) {
             //service_status == 1 maybe has passenger
-            if (taxi_json_array[i].passenger != null) {
-              taxi_json_array[i].has_passenger = "true"
+            if (taxi_json_array[i].service.passenger != null) {
+              taxi_json_array[i].has_passenger = true
             }
             else {
-              taxi_json_array[i].has_passenger = "false"
+              taxi_json_array[i].service.has_passenger = false
             }
             delete taxi_json_array[i].service
             new_taxi_array.push(taxi_json_array[i])
